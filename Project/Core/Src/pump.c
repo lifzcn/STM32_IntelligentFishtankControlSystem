ed4626b7b22e41ae293cb44c9c840a2a684ed2a9
LiFZ <lifzcn@qq.com>
2023-05-04 21:32:20 +0800

    Update some files

--- Project/Core/Src/pump.c
+++ Project/Core/Src/pump.c
@@ -3,11 +3,11 @@
 void addWater()
 {
 	HAL_GPIO_WritePin(Pump1_IN1_GPIO_Port, Pump1_IN1_Pin, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(Pump1_IN3_GPIO_Port, Pump1_IN3_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(Pump2_IN2_GPIO_Port, Pump2_IN2_Pin, GPIO_PIN_RESET);
 }
 
 void subtractWater()
 {
-	HAL_GPIO_WritePin(Pump2_IN2_GPIO_Port, Pump2_IN2_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(Pump1_IN3_GPIO_Port, Pump1_IN3_Pin, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(Pump2_IN4_GPIO_Port, Pump2_IN4_Pin, GPIO_PIN_RESET);
 }
